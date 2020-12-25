d32b7f83a99fc3b7b600e059991bedf746c41691
Pranav Teegavarapu <pranavnt@outlook.com>
2020-12-25 03:26:22 -0800

    started postsJSON.js

---
+++ pages/postsJSON.js
@@ -0,0 +1,21 @@
+const postsJSON = {
+  posts: [
+    {
+      title: "Merry Christmas!",
+      description: "Merry Christmas!",
+      date: "Dec 25, 2020",
+      link:
+        "https://raw.githubusercontent.com/pranavnt/blog/main/posts/2020/12-25%20Merry%20Christmas!.md",
+    },
+    {
+      title: "Welcome to my blog!",
+      description:
+        "Hello world! Welcome to my blog, where I'll write about a bunch of random stuff",
+      date: "Dec 25, 2020",
+      link:
+        "https://raw.githubusercontent.com/pranavnt/blog/main/posts/2020/12-25%20Merry%20Christmas!.md",
+    },
+  ],
+};
+
+export default postsJSON;
