ad5f0b43662ebbebd0f09f09d7d0cc3992dea799
Pranav Teegavarapu <pranavnt@outlook.com>
2020-12-25 16:03:05 -0800

    added back bottom border

--- components/ArticleBlock.js
+++ components/ArticleBlock.js
@@ -2,7 +2,10 @@ import styles from "../styles/Home.module.css";
 
 export default function ArticleBlock(props) {
   return (
-    <div className={styles.ArticleBlock}>
+    <div
+      className={styles.ArticleBlock}
+      style={{ borderBottom: "1px solid #dddddd" }}
+    >
       <a href={props.route}>
         <div>
           <h2>{props.title}</h2>
