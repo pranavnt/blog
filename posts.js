43c8ecc7661e2e6538a8f1345c49f7ffef8bc36d
Pranav Teegavarapu <pranavnt@outlook.com>
2020-12-26 23:45:33 -0800

    testing kobra link

--- posts.js
+++ posts.js
@@ -1,12 +1,13 @@
 const postsJSON = {
   posts: [
     {
-      title: "Merry Christmas!",
-      description: "Merry Christmas!",
-      date: "Dec 25, 2020",
+      title: "Join Kobra!",
+      description:
+        "Basic information about Kobra for anyone interested in contributing!",
+      date: "Dec 26, 2020",
       link:
-        "https://raw.githubusercontent.com/pranavnt/blog/main/posts/2020/12-25-merry-christmas.md",
-      route: "merry-christmas",
+        "https://raw.githubusercontent.com/pranavnt/blog/main/posts/2020/12-26-join-kobra.md",
+      route: "join-kobra",
     },
     {
       title: "Welcome to my blog!",
---
+++ posts/2020/12-26-join-kobra.md
@@ -0,0 +1,9 @@
+# Join Kobra!
+
+### Kobra is an open-source visual programming language (like Scratch) for Machine Learning
+
+## Tech Stack
+
+- Next.js -- currently migrating to Next from React
+- TypeScript ❤️ (for both frontend and backend)
+- GraphQL
